127ab50a64d33d7a7793f0e809c897ed2df87acd
Keith Leon <KeithILeon@gmail.com>
2019-01-22 17:26:43 -0500

    Add Reddit API call

--- src/App.js
+++ src/App.js
@@ -3,22 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      subreddits: [],
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.reddit.com/subreddits/popular/.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+
+          this.setState({
+            subreddits: result.data.children
+          });
+
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
   render() {
+    const{subreddits} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <ul>
+              {subreddits.map(subreddit => (
+                <li key={subreddit.data.display_name}>
+                {subreddit.data.display_name}
+                <br/>
+                {subreddit.data.subscribers}
+                </li>
+              ))}
+            </ul>
         </header>
       </div>
     );
