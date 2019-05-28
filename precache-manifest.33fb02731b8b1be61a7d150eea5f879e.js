b08081a6c3d7fbfc527c22a7e4282391d6db5ad7
Keith Leon <KeithILeon@gmail.com>
2019-05-27 22:38:14 -0400

    Updates

---
+++ precache-manifest.33fb02731b8b1be61a7d150eea5f879e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd30ab3399a939fbc15762f5768be3c3",
+    "url": "/chrn/index.html"
+  },
+  {
+    "revision": "8f0d93cdc41b5badea95",
+    "url": "/chrn/static/css/main.ba1e5da2.chunk.css"
+  },
+  {
+    "revision": "b2838d3c9e4cf4694ddf",
+    "url": "/chrn/static/js/2.ab533eb6.chunk.js"
+  },
+  {
+    "revision": "8f0d93cdc41b5badea95",
+    "url": "/chrn/static/js/main.e7197747.chunk.js"
+  },
+  {
+    "revision": "44abba2aeb8b3e704e49",
+    "url": "/chrn/static/js/runtime~main.93fa8c7a.js"
+  }
+]);
